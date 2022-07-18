1a6a40aa30d2eb1669044a7c0f0fe61d751d4607
Eli Robles <97868399+robleseli@users.noreply.github.com>
2022-07-18 18:17:01 -0500

    Update picksobjectcodeVer2.py

--- picksobjectcodeVer2.py
+++ picksobjectcodeVer2.py
@@ -26,8 +26,49 @@ except TypeError as e:
 print("end")
 
 
-
+#1
 add_pick = printpicks.add_pick(1,1,True)  #positional arg.
+print(add_pick) #no errorss
 
-print(add_pick)
-print("end2")
+#2
+delete_picks = printpicks.delete_picks(1,1,True)
+print(delete_picks)
+
+#3
+#add_pair = printpicks.add_pair(1,1,1)
+#print(add_pair)
+
+#4
+#close_pick = printpicks.close_pick(1,1)
+#print(close_pick)
+#array concat. errors
+
+#5
+#delete_pick = printpicks.delete_pick(1,1,1,1)
+#print(delete_pick)
+#having array concat.errors
+
+#6
+get_sweeps = printpicks.get_sweeps()
+print(get_sweeps)
+
+#7
+#calc_missing_vmax = printpicks.calc_missing_vmax()
+#print(calc_missing_vmax)
+#print("OKAY7")
+
+#8
+#autopick = printpicks.autopick()
+#print(autopick)
+#print("OKAY8")
+
+#9
+display_name = printpicks.display_name()
+print(display_name)
+
+#10
+#is_complete = printpicks.is_complete()
+#print(is_complete)
+
+
+
