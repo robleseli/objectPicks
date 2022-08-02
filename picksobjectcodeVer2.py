9b51a21bebc66372c7f6de8086e0e7099321f630
Eli Robles <97868399+robleseli@users.noreply.github.com>
2022-08-02 09:51:21 -0500

    Update picksobjectcodeVer2.py

--- picksobjectcodeVer2.py
+++ picksobjectcodeVer2.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import sys
 from array import *
 import numpy as np
@@ -15,64 +14,49 @@ print(sys.path)
 
 from pickutils import Picks
 
-np_array1 = np.array([[2, 3],[2,4]])
+np_array1 = np.array([[]])
+np_array2 = np.array([[]])
+np_array3 = np.array([[]])
 
-printpicks = Picks(np_array1, [],[], "pst",
-"product",1,
-"label", "filepath") #setting the last one as None returns a typeError.
-print(printpicks)
-try:
-    printpicks.load()
-except TypeError as e:
-    print(e)
-    raise e
-
-print("end")
-
+picks = Picks(np_array1, np_array2, np_array3, "IRE/CCx/X01a",
+"der",1,
+"bed", "/disk/kea/WAIS/orig/xtra/RTZ2/PIK/rpe1/IRE/CCx/X01a/bed")
+print(picks)
+#print(picks.get_sweeps());  # Errors out because there is no data.
+picks.load()
+print(picks.get_sweeps());
 
 #1
-add_pick = printpicks.add_pick(1,1,True)  #positional arg.
-print(add_pick) #no errorss
+picks.add_pick(1,1,True)  #positional arg.
 
 #2
-delete_picks = printpicks.delete_picks(1,1,True)
-print(delete_picks)
+picks.delete_pick(1,1,True)
 
 #3
-add_pair = printpicks.add_pair(1,1,1)
-print(add_pair)
+picks.add_pair(1,1,1)
 
 #4
-close_pick = printpicks.close_pick(1,1)
-print(close_pick)
-#array concat. errors
+picks.close_pick(1,1)
 
 #5
-delete_pick = printpicks.delete_pick(1,1,1,1)
-print(delete_pick)
-#having array concat.errors
+picks.delete_picks(1,1,1)
 
 #6
-get_sweeps = printpicks.get_sweeps()
+get_sweeps = picks.get_sweeps()
 print(get_sweeps)
-
 #7
-calc_missing_vmax = printpicks.calc_missing_vmax()
+calc_missing_vmax = picks.calc_missing_vmax()
 print(calc_missing_vmax)
 print("OKAY7")
 
 #8
-autopick = printpicks.autopick()
-print(autopick)
+picks.autopick()
 print("OKAY8")
 
 #9
-display_name = printpicks.display_name()
+display_name = picks.display_name()
 print(display_name)
 
 #10
-is_complete = printpicks.is_complete()
+is_complete = picks.is_complete()
 print(is_complete)
-
-
-
