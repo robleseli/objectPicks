84eac42726c7bbe6664710f6537efebfdf3bc373
Eli Robles <97868399+robleseli@users.noreply.github.com>
2022-08-11 14:57:48 -0500

    Update picksobjectcodeVer2.py
    removed the user input feature https://github.com/robleseli/objectPicks/issues/2#issuecomment-1212313838

--- picksobjectcodeVer2.py
+++ picksobjectcodeVer2.py
@@ -18,12 +18,10 @@ from pickutils import Picks
 np_array1 = np.array([[]])
 np_array2 = np.array([[]])
 np_array3 = np.array([[]])
-nameofpickfile = input("enter the name of the pick file you want to use: ") #valid example: IRE/CCx/X01a
-pickfilepath = input("enter the path of the pick file you want to use: ") #valid example: /disk/kea/WAIS/orig/xtra/RTZ2/PIK/rpe1/IRE/CCx/X01a/bed
 
-picks = Picks(np_array1, np_array2, np_array3, nameofpickfile,
+picks = Picks(np_array1, np_array2, np_array3, "IRE/CCx/X01a",
 "der",1,
-"bed", pickfilepath)
+"bed", "IRE-CCx-X01a.bed")
 print(picks)
 #print(picks.get_sweeps());  # Errors out because there is no data.
 picks.load()
@@ -47,17 +45,14 @@ picks.delete_picks(1,1,1)
 #6
 picks.get_sweeps()
 
-
 #7
 picks.calc_missing_vmax()
 
 #8
 picks.autopick()
 
-
 #9
 picks.display_name()
-
 
 #10
 picks.is_complete()
